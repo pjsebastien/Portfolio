725bc848b47c6191cd01d4e404c0009b5f521268
sebastien PIERREJEAN <77431104+pjsebastien@users.noreply.github.com>
2021-01-28 15:45:57 +0400

    Add files via upload

---
+++ Projet portfolio/script.js
@@ -0,0 +1,14 @@
+
+//Menu responsive
+var btn = document.querySelector(".btn");
+var disp = document.querySelector(".menu2")
+
+btn.addEventListener('click', function() {
+    disp.classList.toggle('menu2');
+});
+
+
+
+
+
+
